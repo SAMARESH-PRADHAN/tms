d91e7bb5745dd55edfdfba80f38134f03867f13c
Samaresh Pradhan <pradhansamaresh2002@gmail.com>
2026-01-30 23:25:37 +0530

    Fix Render port binding

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -1,6 +1,8 @@
 from tms_api import app
+import os
 if __name__ == "__main__":
   
-  app.run(host="0.0.0.0", port=5000)
+  port = int(os.environ.get("PORT", 5000))  # fallback to 5000 locally
+  app.run(host="0.0.0.0", port=port)
 
   
